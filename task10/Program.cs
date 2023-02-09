d360222888575f00a2604583a8ad3f7f8298bce4
Nickolay <nik5te@yandex.ru>
2023-02-09 20:24:16 +0300

    вторая цифра 3значного числа

---
+++ task10/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int rem = num % 100;
+int dec = rem / 10;
+if (num < 99 || num > 1000)
+{
+    Console.WriteLine($"Число {num} не трёхзначное");
+}
+else
+{
+  Console.WriteLine($"Вторая цифра числа {num} - {dec}");  
+} 
